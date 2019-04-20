6c0b12f820d1ee1e4730d5254ff2f0ddae900d50
queeroff <39581460+queeroff@users.noreply.github.com>
2019-04-20 18:55:50 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,29 @@
+let alta = document.getElementById('alta');
+let background = document.getElementById('background');
+let title = document.getElementById('title');
+
+
+function coralColor() {
+  alta.src='https://raw.githubusercontent.com/queeroff/s2/master/images/poll/porter.png';
+  background.style.backgroundColor = '#f48c41';
+  title.style.color= '#f48c41';
+}
+
+
+function fuschiaColor() {
+  alta.src='https://raw.githubusercontent.com/queeroff/s2/master/images/poll/blue.png';
+  background.style.backgroundColor = '#41f49d';
+  title.style.color= '#41f49d';
+}
+
+function rosegoldColor() {
+  alta.src='https://raw.githubusercontent.com/queeroff/s2/master/images/poll/kitty.png';
+  background.style.backgroundColor = '#f44242';
+  title.style.color= '#f44242';
+}
+
+function goldColor() {
+  alta.src= 'https://raw.githubusercontent.com/queeroff/s2/master/images/poll/bq.png';
+  background.style.backgroundColor= '#4158f4';
+  title.style.color= '#4158f4';
+}
