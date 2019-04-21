a126b86dc07b475cfafa0fd3806c456c6b00a7c2
queeroff <39581460+queeroff@users.noreply.github.com>
2019-04-20 20:33:47 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,29 +1,29 @@
 let alta = document.getElementById('alta');
 let background = document.getElementById('background');
-let title = document.getElementById('title');
+
 
 
 function coralColor() {
-  alta.src='https://raw.githubusercontent.com/queeroff/s2/master/images/poll/porter.png';
+  alta.src='https://raw.githubusercontent.com/queeroff/s2v/master/porterfin.png';
   background.style.backgroundColor = '#f48c41';
   title.style.color= '#f48c41';
 }
 
 
 function fuschiaColor() {
-  alta.src='https://raw.githubusercontent.com/queeroff/s2/master/images/poll/blue.png';
+  alta.src='https://raw.githubusercontent.com/queeroff/s2v/master/bluefin.png';
   background.style.backgroundColor = '#41f49d';
   title.style.color= '#41f49d';
 }
 
 function rosegoldColor() {
-  alta.src='https://raw.githubusercontent.com/queeroff/s2/master/images/poll/kitty.png';
+  alta.src='https://raw.githubusercontent.com/queeroff/s2v/master/kittyfin.png';
   background.style.backgroundColor = '#f44242';
   title.style.color= '#f44242';
 }
 
 function goldColor() {
-  alta.src= 'https://raw.githubusercontent.com/queeroff/s2/master/images/poll/bq.png';
+  alta.src= 'https://raw.githubusercontent.com/queeroff/s2v/master/bqfin.png';
   background.style.backgroundColor= '#4158f4';
   title.style.color= '#4158f4';
 }
